2e5b45425291c4ec61fd167f1c45f03244009b22
Fredrik Svensson <fs222id@student.lnu.se>
2023-11-28 08:47:31 +0100

    RED: Player position not changing positive along x

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -13,4 +13,16 @@ describe('Player class', () => {
 
     expect(position).toBeDefined()
   })
+
+  it('should change position positively along the x-axis when right arrow is pressed', () => {
+    const player = new Player()
+    const initialPosition = player.getPosition()
+
+    player.moveRight()
+
+    const newPosition = player.getPosition()
+
+    expect(newPosition.x).toBeGreaterThan(initialPosition.x)
+  })
+
 })
