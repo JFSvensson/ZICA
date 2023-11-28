91cb963f908c98db01bef0c1863a3403d8cd8a7f
Fredrik Svensson <fs222id@student.lnu.se>
2023-11-28 08:59:10 +0100

    RED: Fix test to return copy of position

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -16,8 +16,7 @@ describe('Player class', () => {
 
   it('should change position positively along the x-axis when right arrow is pressed', () => {
     const player = new Player()
-    const initialPosition = player.getPosition()
-
+    const initialPosition = { ...player.getPosition() }
     player.moveRight()
 
     const newPosition = player.getPosition()
